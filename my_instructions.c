5115d3b0090455018d955e3bcd582a792079daef
YongHyun-Yi <dldydgus1995@naver.com>
2023-01-26 11:01:29 +0900

    초기 세팅

---
+++ my_instructions.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_instructions.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonghyle <yonghyle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 10:53:59 by yonghyle          #+#    #+#             */
+/*   Updated: 2023/01/26 11:00:35 by yonghyle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sa(void);
+void	sb(void);
+void	ss(void);
+void	pa(void);
+void	pb(void);
+void	ra(void);
+void	rb(void);
+void	rr(void);
+void	rra(void);
+void	rrb(void);
+void	rrr(void);
---
+++ push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonghyle <yonghyle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 10:48:24 by yonghyle          #+#    #+#             */
+/*   Updated: 2023/01/26 10:52:52 by yonghyle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+// # include "libft.h"
+// # include "ft_printf.h"
+
+#endif
